7c309801daed10bfe5f9d7bbc52fd8e9db7216ea
nc5432 <ncasey113@gmail.com>
2023-03-29 15:48:23 -0400

    Started implementing cheese custom properties

--- src/main/java/com/nc543/learningtomod/item/custom/Cheese.java
+++ src/main/java/com/nc543/learningtomod/item/custom/Cheese.java
@@ -1,11 +1,20 @@
 package com.nc543.learningtomod.item.custom;
 
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 public class Cheese extends Item{
 
     public Cheese(Properties p) {
         super(p);
         //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity entity) {
+        // TODO Auto-generated method stub
+        return super.finishUsingItem(stack, level, entity);
     }
 }
