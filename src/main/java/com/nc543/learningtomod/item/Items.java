27b9b7d1d0b2ef0e8d55b6ec2dd0c54ee9cbf8b8
nc543 <89219682+nc5432@users.noreply.github.com>
2023-03-30 14:42:12 -0400

    Adds beskar chestplate item

--- src/main/java/com/nc543/learningtomod/item/Items.java
+++ src/main/java/com/nc543/learningtomod/item/Items.java
@@ -17,6 +17,7 @@ public class Items{
     public static final RegistryObject<Item> BESKAR_INGOT = ITEMS.register("beskar_ingot", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.LEARNING_TAB).fireResistant()));
     public static final RegistryObject<Item> BESKAR_ORE = ITEMS.register("beskar_ore", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.LEARNING_TAB).fireResistant()));
     public static final RegistryObject<Item> BESKAR_HELMET = ITEMS.register("beskar_helmet", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.LEARNING_TAB).fireResistant()));
+    public static final RegistryObject<Item> BESKAR_CHESTPLATE = ITEMS.register("beskar_chestplate", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.LEARNING_TAB).fireResistant()));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
